32e1119c3433c54812b90133d37fc4f78b4458fa
Adar Maori <adarmaori@gmail.com>
2021-04-25 23:31:03 +0300

    setup

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+const Layout = ({children}) => {
+    return (
+        <div>
+            
+            {children}
+            <footer>
+                <h4>powered by caffine and boredom</h4>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
